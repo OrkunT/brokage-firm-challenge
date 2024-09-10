0525a856af3c70ff903124a89326739e3ffc0b0b
OrkunT <34773848+OrkunT@users.noreply.github.com>
2024-09-10 04:36:18 +0300

    Add different port

--- adminservice/src/main/java/com/brokerage/adminservice/domain/event/OrderMatchedEvent.java
+++ adminservice/src/main/java/com/brokerage/adminservice/domain/event/OrderMatchedEvent.java
@@ -2,16 +2,15 @@ package com.brokerage.adminservice.domain.event;
 
 
 import com.brokerage.adminservice.domain.model.Order;
+import lombok.Getter;
 
+@Getter
 public class OrderMatchedEvent {
+    // Getters
     private final Order order;
 
     public OrderMatchedEvent(Order order) {
         this.order = order;
     }
 
-    // Getters
-    public Order getOrder() {
-        return order;
-    }
 }
